bdde9022047b2e78972ccbfca82c12c655aa3ccd
Aderajew <aderajewzewdu780@gmail.com>
2026-06-09 12:45:46 +0300

    Add App Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 // ...
+import LoginPage from "./pages/LoginPage";
+// ...
+<Route path="/login" element={<LoginPage />} />;
 <Route path="/register" element={<RegisterPage />} />;
 import Header from "./components/Header";
 import Home from "./pages/Home";
